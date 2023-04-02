60516eeb65dc1b6e4a2b9d01cc6e8bcf18d3751e
ashank2603 <ashank.priyadarshi@gmail.com>
2023-04-03 00:17:25 +0530

    Login functionality added

--- components/modals/LoginModal.tsx
+++ components/modals/LoginModal.tsx
@@ -3,6 +3,7 @@ import { useCallback, useState } from "react";
 import Input from "../Input";
 import Modal from "../Modal";
 import useRegisterModal from "@/hooks/useRegisterModal";
+import { signIn } from "next-auth/react";
 
 const LoginModal = () => {
     const loginModal = useLoginModal();
@@ -25,7 +26,10 @@ const LoginModal = () => {
         try {
             setIsLoading(true);
 
-            // Add login request here
+            await signIn('credentials', {
+                email,
+                password
+            })
 
             loginModal.onClose();
         } catch(error) {
@@ -33,7 +37,7 @@ const LoginModal = () => {
         } finally {
             setIsLoading(false);
         }
-    },[loginModal]);
+    },[loginModal, email, password]);
 
     const bodyContent = (
         <div className="flex flex-col gap-4">
